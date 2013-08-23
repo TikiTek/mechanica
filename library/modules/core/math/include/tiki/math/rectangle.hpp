f8321a4212f760d9ae5e2ccaac7c6697ba680b2f
Tim Boden <mail@timboden.de>
2013-08-23 22:04:16 +0200

    - implement render targets - fix small issues

--- library/modules/core/math/include/tiki/math/rectangle.hpp
+++ library/modules/core/math/include/tiki/math/rectangle.hpp
@@ -1,47 +1,25 @@
 #ifndef TIKI_RECTANGLE_HPP__
 #define TIKI_RECTANGLE_HPP__
 
-#include "tiki/math/vector2.hpp"
+#include "tiki/base/types.hpp"
 
 namespace tiki
 {
 	struct Rectangle
 	{
-		float x, y, width, height;
+		float x;
+		float y;
+		float width;
+		float height;
 
-		Rectangle( ) :
-			x ( 0 ), y( 0 ), width( 1 ), height( 1 )
+		Rectangle( )
+			: x ( 0.0f ), y( 0.0f ), width( 0.0f ), height( 0.0f )
 		{
-
 		}
 
-		Rectangle( float X, float Y, float Width, float Height ):
-			x( X ), y( Y ), width( Width ), height( Height )
+		Rectangle( float _x, float _y, float _width, float _height )
+			: x( _x ), y( _y ), width( _width ), height( _height )
 		{
-
-		}
-
-		Rectangle( const Rectangle& other )
-			: x( other.x ), y( other.y ), width( other.width ), height( other.height )
-		{
-			x = x;
-		}
-
-		Rectangle& operator=( const Rectangle& other )
-		{
-			x		= other.x;
-			y		= other.y;
-			width	= other.width;
-			height	= other.height;
-			return *this;
-		}
-
-		void mul( const Vector2& scal )
-		{
-			x		*= scal.x;
-			y		*= scal.y;
-			width	*= scal.x;
-			height	*= scal.y;
 		}
 	};
 
