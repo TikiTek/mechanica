e15e084562dea4ecd77a2b6510bb5d3d89eeb04a
Tim Boden <mail@timboden.de>
2013-11-04 00:52:01 +0100

    remove cmake

--- library/buildtools/cmake/scripts/main.cmake
+++
@@ -1,9 +0,0 @@
-
-include(${BUILDTOOLS_SCRIPTS}/macros.cmake)
-include(${BUILDTOOLS_SCRIPTS}/properties.cmake)
-
-include(${BUILDTOOLS_SCRIPTS}/defines.cmake)
-
-include(${BUILDTOOLS_SCRIPTS}/modules.cmake)
-include(${BUILDTOOLS_SCRIPTS}/executable.cmake)
-include(${BUILDTOOLS_SCRIPTS}/unity.cmake)
