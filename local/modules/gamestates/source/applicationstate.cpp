b7d16140a9a2372d22ebc23377db86ae66acccc1
Tim Boden <mail@timboden.de>
2013-12-17 23:53:20 +0100

    factory less progress

--- local/modules/gamestates/source/applicationstate.cpp
+++ local/modules/gamestates/source/applicationstate.cpp
@@ -1,6 +1,12 @@
 
 #include "tiki/gamestates/applicationstate.hpp"
 
+#include "tiki/framework/framework.hpp"
+#include "tiki/graphicsresources/material.hpp"
+#include "tiki/graphicsresources/model.hpp"
+#include "tiki/graphicsresources/shaderset.hpp"
+#include "tiki/graphicsresources/texture.hpp"
+
 namespace tiki
 {
 	void ApplicationState::create()
@@ -13,7 +19,34 @@ namespace tiki
 
 	TransitionState ApplicationState::processTransitionStep( size_t currentStep, bool isCreating, bool isInital )
 	{
-		return TransitionState_Finish;
+		switch ( currentStep )
+		{
+		case ApplicationStateTransitionSteps_RegisterResourceTypes:
+			{
+				ResourceManager& resourceManager = framework::getResourceManager();
+				GraphicsSystem& graphicsSystem = framework::getGraphicsSystem();
+
+				if ( isInital )
+				{
+					Material::registerResourceType( resourceManager );
+					Model::registerResourceType( resourceManager, graphicsSystem );
+					ShaderSet::registerResourceType( resourceManager, graphicsSystem );
+					Texture::registerResourceType( resourceManager, graphicsSystem );
+				}
+				else
+				{
+					Material::unregisterResourceType( resourceManager );
+					Model::unregisterResourceType( resourceManager );
+					ShaderSet::unregisterResourceType( resourceManager );
+					Texture::unregisterResourceType( resourceManager );
+				}
+
+				return TransitionState_Finish;
+			}
+			//break;
+		}
+
+		return TransitionState_Error;
 	}
 
 	void ApplicationState::update()
