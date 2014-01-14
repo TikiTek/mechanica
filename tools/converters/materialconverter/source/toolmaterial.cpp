e4cc6fc9b32a68e153cd1e9641705eacdd31eb17
Tim Boden <mail@timboden.de>
2014-01-14 23:10:19 +0100

    material converter progress

--- tools/converters/materialconverter/source/toolmaterial.cpp
+++ tools/converters/materialconverter/source/toolmaterial.cpp
@@ -1,11 +1,18 @@
 #include "tiki/materialconverter/toolmaterial.hpp"
 
 #include "tiki/base/reflection.hpp"
+#include "tiki/converterbase/converterhelper.hpp"
 #include "tiki/converterbase/resourcewriter.hpp"
 #include "tiki/toolbase/tikixml.hpp"
 
 namespace tiki
 {
+	struct MaterialDataReference
+	{
+		uint			dataOffset;
+		ReferenceKey	referenceKey;
+	};
+
 	ToolMaterial::ToolMaterial()
 	{
 		m_pDataType		= nullptr;
@@ -41,6 +48,7 @@ namespace tiki
 			return false;
 		}
 		m_pDataType = static_cast< const reflection::StructType* >( pTypeBase );
+		m_effectData.create( m_pDataType->getFieldCount() );
 
 		const XmlElement* pValueNode = xml.findFirstChild( "field", pRootNode );
 		while ( pValueNode )
@@ -48,29 +56,30 @@ namespace tiki
 			const XmlAttribute* pNameAtt = xml.findAttributeByName( "name", pValueNode );
 			if ( pNameAtt == nullptr )
 			{
-				TIKI_TRACE_ERROR( "[materialconverter] Could not find 'name' attribute of field.\n" );
+				TIKI_TRACE_ERROR( "[toolmaterial] Could not find 'name' attribute of field.\n" );
 				continue;
 			}
 
 			const XmlAttribute* pTypeAtt = xml.findAttributeByName( "type", pValueNode );
 			if ( pTypeAtt == nullptr )
 			{
-				TIKI_TRACE_ERROR( "[materialconverter] Field with name '%s' has no 'type' attribute.\n", pNameAtt->content );
+				TIKI_TRACE_ERROR( "[toolmaterial] Field with name '%s' has no 'type' attribute.\n", pNameAtt->content );
 				continue;
 			}
 
 			const reflection::FieldMember* pField = m_pDataType->getFieldByName( pNameAtt->content );
 			if ( pField == nullptr )
 			{
-				TIKI_TRACE_ERROR( "[materialconverter] Field with name '%s' is not a member of Type: '%s'.\n", pNameAtt->content, m_pDataType->getName().cStr() );
+				TIKI_TRACE_ERROR( "[toolmaterial] Field with name '%s' is not a member of Type: '%s'.\n", pNameAtt->content, m_pDataType->getName().cStr() );
 				continue;
 			}
 
-			const string typeString = pTypeAtt->content;
-			if ( typeString == "resource" )
-			{
+			MaterialField field;
+			field.name	= pNameAtt->content;
+			field.type	= pTypeAtt->content;
+			field.value	= pValueNode->content;
 
-			}
+			m_effectData.set( pField, field );
 
 			pValueNode = xml.findNext( "field", pValueNode );
 		}
@@ -84,9 +93,49 @@ namespace tiki
 		m_effectData.dispose();
 	}
 
-	bool ToolMaterial::writeResource( ResourceWriter& writer ) const
+	ReferenceKey ToolMaterial::writeResource( ResourceWriter& writer ) const
 	{
-		return false;
+		void* pData = m_pDataType->createInstance();
+
+		List< MaterialDataReference > references;
+		for (uint i = 0u; i < m_effectData.getCount(); ++i)
+		{
+			const MaterialFieldMap::Pair& kvp = m_effectData.getPairAt( i );
+
+			uint8 workingMemory[ 16u ];
+			TIKI_ASSERT( TIKI_COUNT( workingMemory ) >= kvp.key->getTypeInfo().getType()->getSize() );
+			if ( kvp.value.type == "resource" )
+			{
+				MaterialDataReference reference;
+				reference.dataOffset = kvp.key->getOffset();
+				if ( readResourceReference( writer, kvp.value.value, reference.referenceKey ) )
+				{
+					references.add( reference );
+				}
+			}
+			else if ( kvp.value.type == "integer" )
+			{
+				//static_cast< uint64* >
+			}
+			else if ( kvp.value.type == "float" )
+			{
+
+			}
+			else
+			{
+				TIKI_TRACE_ERROR( "[toolmaterial] field type '%s' not supported.\n", kvp.value.type.cStr() );
+				continue;
+			}
+
+			kvp.key->setValue( pData, workingMemory );
+		} 
+
+		writer.openDataSection( 0u, AllocatorType_MainMemory );
+		const ReferenceKey key = writer.addDataPoint();
+		writer.writeData( pData, m_pDataType->getSize() );
+		writer.closeDataSection();
+
+		return key;
 	}
 
 }
