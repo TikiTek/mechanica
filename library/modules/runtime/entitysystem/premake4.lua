dfebe44d7199be5ecaac9a80459835daf97d47a3
Tim Boden <mail@timboden.de>
2014-03-30 01:30:12 +0100

    add files

---
+++ library/modules/runtime/entitysystem/premake4.lua
@@ -0,0 +1,9 @@
+-- library/modules/runtime/entitysystem
+
+local module = Module:new( "entitysystem" );
+
+module:add_files( "include/**/*.hpp" );
+module:add_files( "source/*.*" );
+module:add_include_dir( "include" );
+
+module:add_dependency( "componentsystem" );
