0f0122d6d11ad78481623f01d4a0a707999d1064
Tim Boden <mail@timboden.de>
2014-03-30 01:30:12 +0100

    add files

---
+++ modules/runtime/entitysystem/include/tiki/entitysystem/entitysystem.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#ifndef __TIKI_ENTITYSYSTEM_HPP_INCLUDED__
+#define __TIKI_ENTITYSYSTEM_HPP_INCLUDED__
+
+#include "tiki/components/componentstorage.hpp"
+#include "tiki/components/typeregister.hpp"
+
+namespace tiki
+{
+	struct EntityTemplate;
+
+	struct EntitySystemParameters
+	{
+		uint	typeRegisterMaxCount;
+		uint	storageChunkSize;
+		uint	storageChunkCount;
+	};
+
+	class EntitySystem
+	{
+		TIKI_NONCOPYABLE_CLASS( EntitySystem );
+
+	public:
+
+		EntitySystem();
+		~EntitySystem();
+
+		bool		create( const EntitySystemParameters& parameters );
+		void		dispose();
+
+		bool		registerComponentType( ComponentBase* pComponent );
+		void		unregisterComponentType( ComponentBase* pComponent );
+
+		EntityId	createEntityFromTemplate( const EntityTemplate& entityTemplate );
+		void		destroyEntity( EntityId entityId );
+		
+	private:
+
+		ComponentTypeRegister	m_typeRegister;
+		ComponentStorage		m_storage;
+
+
+
+	};
+}
+
+#endif // __TIKI_ENTITYSYSTEM_HPP_INCLUDED__
