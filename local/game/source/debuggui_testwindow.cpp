769b91eeb0d2584f14d5ad7808e92d7650c22f9c
Tim Boden <mail@timboden.de>
2014-04-26 15:56:24 +0200

    more debug gui layouts

--- local/game/source/debuggui_testwindow.cpp
+++ local/game/source/debuggui_testwindow.cpp
@@ -5,25 +5,50 @@ namespace tiki
 {
 	void DebugGuiTestWindow::create( DebugGui& debugGui )
 	{
-		DebugGuiWindow::create( debugGui, "Test Window", m_layout );
+		DebugGuiWindow::create( debugGui, "Test Window", m_baseLayout );
+		
+		for (uint i = 0u; i < TIKI_COUNT( m_labels ); ++i)
+		{
+			char text[] = "Label0";
+			text[ 5u ] = (char)( '0' + i );
+
+			m_labels[ i ].create( text );
+			m_labelLayout.addChildControl( &m_labels[ i ] );
+		} 
 
 		for (uint i = 0u; i < TIKI_COUNT( m_buttons ); ++i)
 		{
 			char text[] = "Button0";
 			text[ 6u ] = (char)( '0' + i );
 
+			if ( i == 3 )
+			{
+				m_bottomLayout.addChildControl( &m_labelLayout );
+			}
+
 			m_buttons[ i ].create( text );
-			m_layout.addChildControl( &m_buttons[ i ] );
+			m_bottomLayout.addChildControl( &m_buttons[ i ] );
 		} 
+
+		m_baseLayout.addChildControl( &m_bottomLayout );
 	}
 
 	void DebugGuiTestWindow::dispose()
 	{
+		for (uint i = 0u; i < TIKI_COUNT( m_labels ); ++i)
+		{
+			m_labelLayout.removeChildControl( &m_labels[ i ] );
+			m_labels[ i ].dispose();
+		}
+
 		for (uint i = 0u; i < TIKI_COUNT( m_buttons ); ++i)
 		{
-			m_layout.removeChildControl( &m_buttons[ i ] );
+			m_bottomLayout.removeChildControl( &m_buttons[ i ] );
 			m_buttons[ i ].dispose();
 		}
+
+		m_bottomLayout.removeChildControl( &m_labelLayout );
+		m_baseLayout.removeChildControl( &m_bottomLayout );
 
 		DebugGuiWindow::dispose();
 	}
