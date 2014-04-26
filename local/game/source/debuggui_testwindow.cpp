ebfb659573cc36a3c74ef593232d0893ec793a5d
Tim Boden <mail@timboden.de>
2014-04-26 02:33:36 +0200

    debug gui progress

--- local/game/source/debuggui_testwindow.cpp
+++ local/game/source/debuggui_testwindow.cpp
@@ -5,11 +5,26 @@ namespace tiki
 {
 	void DebugGuiTestWindow::create( DebugGui& debugGui )
 	{
-		DebugGuiWindow::create( debugGui, "Test Window" );
+		DebugGuiWindow::create( debugGui, "Test Window", m_layout );
+
+		for (uint i = 0u; i < TIKI_COUNT( m_buttons ); ++i)
+		{
+			char text[] = "Button0";
+			text[ 6u ] = '0' + i;
+
+			m_buttons[ i ].create( text );
+			m_layout.addChildControl( &m_buttons[ i ] );
+		} 
 	}
 
 	void DebugGuiTestWindow::dispose()
 	{
+		for (uint i = 0u; i < TIKI_COUNT( m_buttons ); ++i)
+		{
+			m_layout.removeChildControl( &m_buttons[ i ] );
+			m_buttons[ i ].dispose();
+		}
+
 		DebugGuiWindow::dispose();
 	}
 }
