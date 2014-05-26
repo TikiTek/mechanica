081ece542b58253847ffa0a6e00d436db56dcd33
Tim Boden <mail@timboden.de>
2014-05-26 23:33:45 +0200

    finalize coins

--- modules/runtime/physics/source/physicsworld.cpp
+++ modules/runtime/physics/source/physicsworld.cpp
@@ -32,10 +32,7 @@ namespace tiki
 
 		virtual	btScalar addSingleResult( btManifoldPoint& cp, const btCollisionObjectWrapper* colObj0Wrap, int partId0, int index0, const btCollisionObjectWrapper* colObj1Wrap, int partId1, int index1 )
 		{
-			TIKI_TRACE( "test\n" );
-
 			m_collides = true;
-
 			return 0.0f;
 		}
 
