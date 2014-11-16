dec18772a0572ec69f4515ec09366011d0b4cd17
Tim Boden <mail@timboden.de>
2014-11-16 01:54:31 +0100

    remove unused code

--- library/modules/runtime/graphics/source/win_d3d12/uploadheap_d3d12.hpp
+++ library/modules/runtime/graphics/source/win_d3d12/uploadheap_d3d12.hpp
@@ -30,16 +30,12 @@ namespace tiki
 
 		ID3D12Resource*	getBuffer() const { return m_pBuffer; }
 
-		ID3D12Resource*	beginReading();
-		void			endReading();
-
-
 	private: // friend
 
 		UploadHeapD3d12();
 		~UploadHeapD3d12();
 
-		bool	create( ID3D12Device* pDevice, ID3D12CommandList* pCommandList, uint size, uint maxAllocationCount = 128u );
+		bool	create( ID3D12Device* pDevice, uint size, uint maxAllocationCount = 128u );
 		void	dispose();
 
 		void	finalizeFrame( uint currentFrameIndex );
@@ -52,8 +48,6 @@ namespace tiki
 			uint		frameIndex;
 			uint		size;
 		};
-
-		ID3D12CommandList*		m_pCommandList;
 
 		ID3D12Resource*			m_pBuffer;
 		uint8*					m_pDataStart;
