5f4b42fc5e9683294d336165e0301bc880dc9829
Tim Boden <mail@timboden.de>
2015-10-08 18:52:11 +0200

    forgotten files

---
+++ library/modules/core/container/container.lua
@@ -0,0 +1,12 @@
+-- library/modules/core/container
+
+local module = Module:new( "container" );
+
+module:add_files( "source/*.*" );
+module:add_files( "source/win/*.*" );
+module:add_files( "include/**/*.hpp" );
+module:add_include_dir( "include" );
+
+module:add_dependency( "base" );
+module:add_dependency( "threading" );
+module:add_dependency( "dbalsterxml" );
---
+++ library/modules/core/io/include/tiki/io/memorystream.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#ifndef TIKI_MEMORYSTREAM_HPP
+#define TIKI_MEMORYSTREAM_HPP
+
+#include "tiki/io/datastream.hpp"
+
+#include "tiki/base/types.hpp"
+
+namespace tiki
+{
+	class MemoryStream : public DataStream
+	{
+	public:
+
+					MemoryStream();
+					MemoryStream( const MemoryStream& copy );
+		virtual		~MemoryStream();
+
+		void		operator=( const MemoryStream& copy );
+
+		void				create( FileSize capacity = 0u );
+		void				create( const void* pData, FileSize length );
+		virtual void		dispose() TIKI_OVERRIDE TIKI_FINAL;
+
+		bool				isOpen() const;
+		const void*			getData() const;
+
+		virtual FileSize	read( void* pTargetData, FileSize bytesToRead ) const TIKI_OVERRIDE TIKI_FINAL;
+
+		virtual FileSize	write( const void* pSourceData, FileSize bytesToWrite ) TIKI_OVERRIDE TIKI_FINAL;
+		void				writeAlignment( FileSize alignment );
+
+		virtual FileSize	getPosition() const TIKI_OVERRIDE TIKI_FINAL;
+		virtual void		setPosition( FileSize position ) TIKI_OVERRIDE TIKI_FINAL;
+		virtual FileSize	seekPosition( FileOffset offset, DataStreamSeek method = DataStreamSeek_Current ) TIKI_OVERRIDE TIKI_FINAL;
+
+		virtual FileSize	getLength() const TIKI_OVERRIDE TIKI_FINAL;
+		virtual void		setLength( FileSize length ) TIKI_OVERRIDE TIKI_FINAL;
+
+	private:
+
+		uint8*				m_pData;
+
+		FileSize			m_pos;
+		FileSize			m_length;
+		FileSize			m_capacity;
+
+		bool				increaseBuffer( FileSize requiredSize );
+
+	};
+}
+
+#endif // TIKI_MEMORYSTREAM_HPP
