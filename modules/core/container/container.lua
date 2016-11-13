b699b09aba48dbfd877604fbcb9fc796806fa2b3
Tim Boden <mail@timboden.de>
2016-11-13 15:32:29 +0100

    fix editor build

--- modules/core/container/container.lua
+++ modules/core/container/container.lua
@@ -4,7 +4,7 @@ local module = Module:new( "container" );
 
 module:add_files( "source/*.*" );
 module:add_files( "include/**/*.hpp" );
-module:add_files( "container.lua" );
+module:add_files( "*.lua" );
 module:add_include_dir( "include" );
 
 module:add_dependency( "core/base" );
