48e89098375bf3b0a9f46ca06e9dfe2b14f2fca0
Tim Boden <mail@timboden.de>
2017-10-31 17:36:28 +0100

    fix uieditor build

--- engine/tools/uieditor/project/genie.lua
+++ engine/tools/uieditor/project/genie.lua
@@ -2,4 +2,19 @@
 
 include "../../../buildtools/genie_scripts"
 
-finalize( "uieditor", { find_project( "uieditor" ) } );
+dofile( "../uieditor.lua" );
+
+local project = Project:new(
+	"uieditor",
+	"ce188937-8610-427a-9397-fc75e2d584c3",
+	{ "x32", "x64" },
+	{ "Debug", "Release" },
+	find_module( 'uieditor' ),
+	ProjectTypes.windowApplication
+);
+
+local solution = Solution:new( "uieditor" );
+solution:add_project( project );
+--solution:add_project( find_project( "converterlibrary" ) );
+
+solution:finalize();
