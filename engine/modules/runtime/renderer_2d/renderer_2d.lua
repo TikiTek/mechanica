33d95e10497288403f512242b8dc2c205c89e1dd
Tim Boden <mail@timboden.de>
2018-11-13 19:09:58 +0100

    rename more modules

---
+++ engine/modules/runtime/renderer_2d/renderer_2d.lua
@@ -0,0 +1,19 @@
+local module = Module:new( "renderer_2d_shader" );
+
+module:add_files( "source/shader/*.*" );
+module:add_shader_dir( "source" );
+
+module:add_dependency( "graphics_shader" );
+
+local module = Module:new( "renderer_2d" );
+
+module:add_files( "source/*.*" );
+module:add_files( "include/**/*.hpp" );
+module:add_files( "*.lua" );
+module:add_include_dir( "include" );
+
+module:add_dependency( "runtime/graphics" );
+module:add_dependency( "runtime/graphics_resources" );
+module:add_dependency( "runtime/renderer_2d_shader" );
+module:add_dependency( "runtime/render_effects" );
+
