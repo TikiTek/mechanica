8ade6cb5869c0512e6212c5b18f73f91b19bf842
Tim Boden <mail@timboden.de>
2019-04-21 00:31:57 +0200

    new state interface

---
+++ modules/runtime/game_flow/source/game_flow_state.cpp
@@ -0,0 +1,51 @@
+#include "tiki/game_flow/game_flow_state.hpp"
+
+#include "tiki/resource/resource_request_pool.hpp"
+
+namespace tiki
+{
+	GameFlowState::GameFlowState()
+	{
+	}
+
+	GameFlowState::~GameFlowState()
+	{
+	}
+
+	TransitionState GameFlowState::processTransitionStep( size_t currentStep, bool isCreating, bool isInital )
+	{
+		if( isCreating )
+		{
+			return processCreationStep( currentStep, isInital );
+		}
+		else
+		{
+			return processDestructionStep( currentStep, isInital );
+		}
+	}
+
+	TransitionState GameFlowState::processCreationStep( size_t currentStep, bool isInital )
+	{
+		return TransitionState_Finish;
+	}
+
+	TransitionState GameFlowState::processDestructionStep( size_t currentStep, bool isInital )
+	{
+		return TransitionState_Finish;
+	}
+
+	TransitionState GameFlowState::waitForResources( const ResourceRequestPool& resourceRequestPool ) const
+	{
+		if( resourceRequestPool.isFinish() )
+		{
+			if( resourceRequestPool.hasError() )
+			{
+				return TransitionState_Error;
+			}
+
+			return TransitionState_Finish;
+		}
+
+		return TransitionState_InProcess;
+	}
+}
