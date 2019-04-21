a4a2a14a605736eefb0d79d516c6395b97b23f94
Tim Boden <mail@timboden.de>
2019-04-21 22:50:07 +0200

    fix code generator build

--- engine/modules/tool/tool_generic_data/tool_generic_data.lua
+++ engine/modules/tool/tool_generic_data/tool_generic_data.lua
@@ -1,8 +1,9 @@
 local module = Module:new();
 
+module:add_files( "*.lua" );
+
 module:add_files( "source/*.*" );
 module:add_files( "include/**/*.hpp" );
-module:add_files( "*.lua" );
 module:add_include_dir( "include" );
 
 module:add_dependency( "tool/tool_base" );
