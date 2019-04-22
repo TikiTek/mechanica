8eda6150b9292b65f2d15a76f8d09307d2819f49
Tim Boden <mail@timboden.de>
2019-04-22 04:58:02 +0200

    more natvis

--- modules/core/resource/resource.lua
+++ modules/core/resource/resource.lua
@@ -1,14 +1,14 @@
--- library/modules/runtime/resource
-
-local module = Module:new( "resource" );
+local module = Module:new();
 
 module:set_define( "TIKI_RESOUCE_ENABLE_CONVERTER", "TIKI_ON", "Debug" );
 module:set_define( "TIKI_RESOUCE_ENABLE_CONVERTER", "TIKI_ON", "Release" );
 module:set_define( "TIKI_RESOUCE_ENABLE_CONVERTER", "TIKI_OFF", "Master" );
 
+module:add_files( "*.lua" );
+module:add_files( "*.natvis" );
+
 module:add_files( "source/*.*" );
 module:add_files( "include/**/*.hpp" );
-module:add_files( "resource.lua" );
 module:add_include_dir( "include" );
 
 module:add_dependency( "core/container" );
