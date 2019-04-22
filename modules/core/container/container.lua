b5e12d05fa11ba50413f7786e1a8dc073540f297
Tim Boden <mail@timboden.de>
2019-04-22 04:58:56 +0200

    lua

--- modules/core/container/container.lua
+++ modules/core/container/container.lua
@@ -1,11 +1,10 @@
--- library/modules/core/container
+local module = Module:new();
 
-local module = Module:new( "container" );
+module:add_files( "*.lua" );
+module:add_files( "*.natvis" );
 
 module:add_files( "source/*.*" );
 module:add_files( "include/**/*.hpp" );
-module:add_files( "*.lua" );
-module:add_files( "*.natvis" );
 module:add_include_dir( "include" );
 
 module:add_dependency( "core/base" );
