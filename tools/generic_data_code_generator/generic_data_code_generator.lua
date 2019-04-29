000d1694118a791601851671fe98be204d00395e
Tim Boden <mail@timboden.de>
2019-04-29 23:02:39 +0200

    install code generator automatically

--- tools/generic_data_code_generator/generic_data_code_generator.lua
+++ tools/generic_data_code_generator/generic_data_code_generator.lua
@@ -2,6 +2,7 @@ local module = Module:new( "generic_data_code_generator" );
 
 module:add_files( "source/*.*" );
 module:add_files( "*.lua" );
+module:add_files( "project/*.lua" );
 
 module:add_dependency( "config" );
 module:add_dependency( "tool/tool_base" );
