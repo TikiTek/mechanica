844cfa6a819c7f2d177af17718490947ab021725
Tim Boden <mail@timboden.de>
2020-02-12 00:24:34 +0100

    better random

--- local/modules/runtime/mechanica_components/include/tiki/mechanica_components/breakable_component.hpp
+++ local/modules/runtime/mechanica_components/include/tiki/mechanica_components/breakable_component.hpp
@@ -2,6 +2,7 @@
 
 #include "tiki/components/component.hpp"
 
+#include "tiki/base/random.hpp"
 #include "tiki/container/array_view.hpp"
 #include "tiki/math/vector.hpp"
 #include "tiki/resource/resource_file.hpp"
@@ -30,9 +31,9 @@ namespace tiki
 		bool				create( EntitySystem& entitySystem, Physics2dWorld& physicsWorld, const Transform2dComponent& transformComponent, const Physics2dBodyComponent& bodyComponent, const SpriteComponent& spriteComponent, const PolygonComponent& polygonComponent );
 		void				dispose();
 
-		void				update( double deltaTime ) const;
+		void				update( double deltaTime );
 
-		void				breakBody( BreakableComponentState* pState ) const;
+		void				breakBody( BreakableComponentState* pState );
 
 	protected:
 
@@ -44,13 +45,15 @@ namespace tiki
 		EntitySystem*						m_pEntitySystem;
 		Physics2dWorld*						m_pPhysicsWorld;
 
+		RandomNumberGenerator				m_random;
+
 		const Transform2dComponent*			m_pTransformComponent;
 		const Physics2dBodyComponent*		m_pBodyComponent;
 		const SpriteComponent*				m_pSpriteComponent;
 		const PolygonComponent*				m_pPolgonComponent;
 
 		void								breakToStaticFragmentEntities( const ComponentEntityIterator& parentEntityComponentIterator, const BreakableComponentState* pParentState ) const;
-		void								breakToCuttedFragmentEntities( const ComponentEntityIterator& parentEntityComponentIterator, const BreakableComponentState* pParentState ) const;
+		void								breakToCuttedFragmentEntities( const ComponentEntityIterator& parentEntityComponentIterator, const BreakableComponentState* pParentState );
 
 		void								createChildBreakable( uint32 layerId, const ArrayView< float2 >& polygonPoints, const ComponentEntityIterator& parentEntityComponentIterator, const BreakableComponentState* pParentState ) const;
 	};
