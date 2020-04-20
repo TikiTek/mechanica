585fbc3cbbd8968f561ed8ea773e7600a40172f7
Tim Boden <mail@timboden.de>
2020-04-20 23:54:42 +0200

    asset build

--- build_assets.ps1
+++ build_assets.ps1
@@ -1,1 +1,19 @@
-Get-ChildItem -Recurse
+$start_time = Get-Date
+$asset_converter = Join-Path -Path $PSScriptRoot -ChildPath "library\tools\command_line_converter\build\x64\Release\command_line_converter.exe"
+
+Echo "Building Assets..."
+Invoke-Expression $asset_converter
+
+$failed = $false
+if($LASTEXITCODE -ne 0) {
+	Echo "Failed to Assets"
+	$failed = $true
+}
+
+$build_time = New-Timespan -Start $start_time -End (Get-Date)
+$result = "Asset Build done after {0:c}." -f $build_time
+echo $result
+
+if ($failed) {
+	exit 1
+}
